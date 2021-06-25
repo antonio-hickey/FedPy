7e920e6b51cc4bedb9e2faf619113c970f0e75e5
Antonio Hickey <antoniohickey99@gmail.com>
2021-06-24 23:22:21 -0400

    Temporary Open Market Operations

--- TOMO/tomo.py
+++ TOMO/tomo.py
@@ -5,14 +5,15 @@ Sourced: New York Federal Reserve (https://markets.newyorkfed.org)
 
 Output Data:
     - recent_rp():
-        - {Date,OpType,OpID,Maturity,Method,Treasuries,Agencies,MortBack,Total}
+        - {Date,OpType,OpId,Maturity,Method,Treasuries,Agencies,MortBack,Total}
+    - recent_rrp():
+        - {Date,OpType,OpId,Maturity,Method,TreasuryCollateral,Yield}
 """
 
 # Import Modules
 from utils.date_handler import DatesHandler as datesHand
 from bs4 import BeautifulSoup as bs
 import requests as req
-import pandas as pd
 
 class TOMO:
 
@@ -59,3 +60,44 @@ class TOMO:
 
         # Return dictionary
         return d
+
+    """
+        Returns a dictionary of most
+        recent Reverse Repo Operations.
+    """
+    def recent_rrp():
+        if (int(datesHand.time()[:2])) > 14:
+            URL = "https://websvcgatewayx2.frbny.org/autorates_tomo_external/services/v1_0/tomo/retrieveXml"
+        else:
+            URL = "https://websvcgatewayx2.frbny.org/autorates_tomo_external/services/v1_0/tomo/retrieveXmlLastN?n=2"
+        # Making Soup
+        page = req.get(URL) # Request Page
+        soup = bs(page.content,"lxml") # Parse page as XML
+        
+        # Types of operations inside group tag
+        groups = soup.findAll("ns2:group")
+        
+        # Reverse Repo Operations
+        RRP = groups[1]
+        op_id = RRP['operationid']
+        deal_date = RRP['dealdate']
+        maturity_date = RRP['maturitydate']
+        auction_method = RRP['auctionmethod']
+        sections = RRP.findAll('ns2:section')
+        treasury_collateral = sections[0]
+        value = treasury_collateral.find("ns2:totalpropositionsaccepted")['value']
+        rate = treasury_collateral.find("ns2:awardrate")['value']
+
+        # Render dictionary
+        d = {
+            'Deal Date': deal_date,
+            'Operation Type': "Reverse Repo",
+            'Operation ID': op_id,
+            'Maturity Date': maturity_date,
+            'Auction Method': auction_method,
+            'Treasury Collateral': value,
+            'Yield': rate,
+        }
+        
+        # Return dictionary above
+        return d 

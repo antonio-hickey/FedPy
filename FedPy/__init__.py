a98699b1ae4e0d398f348766af38a9104ab114e1
Antonio Hickey <antoniohickey99@gmail.com>
2021-11-04 16:40:50 -0400

    build: added new feat modules to init

--- FedPy/__init__.py
+++ FedPy/__init__.py
@@ -2,6 +2,12 @@ from typing import Optional
 
 import pandas as pd
 
+from FedPy.Payment_Systems.ACH import ACH  # noqa
+from FedPy.Payment_Systems.Check_Services import Check_Services  # noqa
+from FedPy.Payment_Systems.Currency import Currency  # noqa
+from FedPy.Payment_Systems.FedWire import FedWire  # noqa
+from FedPy.Payment_Systems.National_Settlement_Service import NSS  # noqa
+
 from .FOMC_Statement.statements import Statements  # noqa
 from .FRED.fred import Fred
 from .SOMA_Portfolio.soma import SOMA  # noqa

47fb826fa47c5d9f10938638e87d8fe25e1fb8d9
Antonio Hickey <antoniohickey99@gmail.com>
2021-11-04 18:57:37 -0400

    fix: return dataframe not object

--- FedPy/Payment_Systems/National_Settlement_Service.py
+++ FedPy/Payment_Systems/National_Settlement_Service.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests as req
 from bs4 import BeautifulSoup as bs
 
@@ -9,7 +10,6 @@ class NSS:
     def __init__(self) -> None:
         self.url_base = 'https://www.frbservices.org/resources/financial-services/national-settlement-service/'
         self.url = f'{self.url_base}volume-value-stats/annual-stats.html'
-        util.to_DataFrame(self.get_data())
 
     def get_data(self) -> list:
         """Helper method to fetch data."""
@@ -20,3 +20,7 @@ class NSS:
         data = [i.text for i in response.findAll("td")]
         rows = [data[i:i+8] for i in range(0, len(data), 8)]
         return [columns, years, rows]
+
+    def fetch(self) -> pd.DataFrame:
+        """Return DataFrame of National Settlement Service data."""
+        return util.to_DataFrame(self.get_data())
